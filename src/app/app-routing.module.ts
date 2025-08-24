f4a55284075e6f6aa1bc97fc77a3918384792894
Aman Jha <amanj@pronnel.com>
2025-08-24 21:57:47 +0530

    made routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatlayoutComponent } from './chatlayout/chatlayout.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import { LoginGuard } from './login.guard';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  {path: 'signup', component: SignupComponent, canActivate: [LoginGuard]},
+  {path: 'chat', component: ChatlayoutComponent , canActivate: [AuthGuard]},
+  {path: '', redirectTo: 'chat', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
